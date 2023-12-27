8b76f27f56ba7e73c26aa6e6b7ad847609aed363
reciokaue <kaue.recio2@gmail.com>
2023-12-27 08:40:43 -0300

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { Button } from './components/ui/button'
+
 export function App() {
-  return <h1>pizza shop</h1>
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Button>Enviar</Button>
+    </div>
+  )
 }
