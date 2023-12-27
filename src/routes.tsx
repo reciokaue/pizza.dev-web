57cafa3dfcd8fb031dca9da78c102d68a1887902
reciokaue <kaue.recio2@gmail.com>
2023-12-27 09:37:13 -0300

    routes & layout

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,8 +2,18 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
+import { AppLayout } from './pages/layouts/app'
+import { AuthLayout } from './pages/layouts/auth'
 
 export const router = createBrowserRouter([
-  { path: '/', element: <Dashboard /> },
-  { path: '/sign-in', element: <SignIn /> },
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [{ path: '/', element: <Dashboard /> }],
+  },
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [{ path: '/sign-in', element: <SignIn /> }],
+  },
 ])
