f38dfbae592d72cb4fe60938432db3d4b82932e8
reciokaue <kaue.recio2@gmail.com>
2024-06-27 11:13:02 -0300

    order table loading

--- src/pages/app/orders/index.tsx
+++ src/pages/app/orders/index.tsx
@@ -15,6 +15,7 @@ import {
 
 import { OrderTableFilters } from './order-table-filters'
 import { OrderTableRow } from './order-table-row'
+import { OrderTableSkeleton } from './order-table-skeleton'
 
 export function Orders() {
   const [searchParams, setSearchParams] = useSearchParams()
@@ -28,7 +29,7 @@ export function Orders() {
     .transform((page) => page - 1)
     .parse(searchParams.get('page') ?? '1')
 
-  const { data: result } = useQuery({
+  const { data: result, isLoading: isLoadingOrders } = useQuery({
     queryKey: ['orders', pageIndex, orderId, customerName, status],
     queryFn: () => getOrders({ pageIndex, orderId, customerName, status }),
   })
@@ -45,7 +46,7 @@ export function Orders() {
       <Helmet title="Pedidos" />
 
       <div className="flex flex-col gap-4">
-        <h1 className="text-3xl font-bold tracking-tighter">Pedidos</h1>
+        <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
         <div className="space-y-2.5">
           <OrderTableFilters />
 
@@ -55,7 +56,7 @@ export function Orders() {
                 <TableRow>
                   <TableHead className="w-[64px]"></TableHead>
                   <TableHead className="w-[140px]">Identificador</TableHead>
-                  <TableHead className="w-[10px]">Realizado há</TableHead>
+                  <TableHead className="w-[180px]">Realizado há</TableHead>
                   <TableHead className="w-[140px]">Status</TableHead>
                   <TableHead>Cliente</TableHead>
                   <TableHead className="w-[140px]">Total do pedido</TableHead>
@@ -64,6 +65,7 @@ export function Orders() {
                 </TableRow>
               </TableHeader>
               <TableBody>
+                {isLoadingOrders && <OrderTableSkeleton />}
                 {result &&
                   result.orders.map((order) => (
                     <OrderTableRow order={order} key={order.orderId} />
