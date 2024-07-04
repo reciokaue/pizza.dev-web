acdb3cb878c332149eed0fd62af6b6939b18d65d
reciokaue <kaue.recio2@gmail.com>
2024-07-04 12:08:25 -0300

    sign in mock

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,9 @@
 import { coerce, z } from 'zod'
 
 const envSchema = z.object({
-  VITE_API_URL: z.string().url(),
+  VITE_API_URL: z.string(),
   VITE_ENABLE_API_DELAY: coerce.boolean(),
+  MODE: z.enum(['production', 'development', 'test']),
 })
 
 export const env = envSchema.parse(import.meta.env)
